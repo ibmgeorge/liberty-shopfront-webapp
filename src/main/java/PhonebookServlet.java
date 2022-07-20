9e7a14b756263b2cf5f5588c14a09cca880a9245
ibmgeorge <ypge@au1.ibm.com>
2022-07-20 13:08:33 +1000

    add servlet / redesign interface / multiapi / pom

--- src/main/java/PhonebookServlet.java
+++ src/main/java/PhonebookServlet.java
@@ -17,18 +17,17 @@ import jakarta.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AccountServiceServlet
  */
-@WebServlet("/accountServlet")
-public class AccountServiceServlet extends HttpServlet {
+@WebServlet("/phonebookServlet")
+public class PhonebookServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private String baseUrl = "https://s0w1.duckdns.org:9445/queryacc/account/";
+	private String baseUrl = "https://s0w1.duckdns.org:9445/phonebook/contact/";
 	private String authHeader = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiQmVhcmVyIiwiYXVkIjoiekNFRSIsInN1YiI6InlwZ2VAYXUxLmlibS5jb20iLCJyZWFsbSI6IkFEQ0RQTCIsImlzcyI6ImxpYmVydHkiLCJleHAiOjE2ODE0Nzk0NjksImlhdCI6MTU4MTQ3MjI2OX0.KJlE-wS1N48cWz1F1uDxpSN7uR9fb4uFGxY3o31OxuoQRFjYTXiaadeDzhmBrz0LK30bdi2oGZlfs0DEhFIkC8iwJlKRS4-bMudTLK37E_UW6T6EhjFmO-X8DUNJeiKVEz0rc982QFoGQLCm7T7-YT7HNqId2RFPe1JKWebJfH-zepME-R6CNpsu7f3ZWnJsopCz2ewuOtt72xtozf6ZnAXTUM46bp60vUbh7btha8C9XNHxq2pIKOJ6sJfcWjqjhA9M7oMRVYO_ml6Gpk6iRL_0HagMytjVEg6Veu7888f4K5IzQJaNQUTFVqE3S17o7Wn8dcCApf8D3CqM78WHag";
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public AccountServiceServlet() {
+	public PhonebookServlet() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -37,10 +36,10 @@ public class AccountServiceServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String accountID = request.getParameter("account");
-		if (accountID == null || "".equalsIgnoreCase(accountID))
-			accountID = "[EMPTY]";
-		URL endpoint = new URL(baseUrl + accountID);
+		String lastName = request.getParameter("lname");
+		if (lastName == null || "".equalsIgnoreCase(lastName))
+			lastName = "NOTFOUND";
+		URL endpoint = new URL(baseUrl + lastName);
 		HttpURLConnection connection = (HttpURLConnection) endpoint.openConnection();
 		connection.setRequestMethod("GET");
 		connection.setRequestProperty("Content-Type", "application/json");
@@ -50,17 +49,30 @@ public class AccountServiceServlet extends HttpServlet {
 			BufferedReader in = new BufferedReader(new InputStreamReader(
 					connection.getInputStream()));
 			JsonReader jsonReader = Json.createReader(in);
-			JsonObject jsonObject = jsonReader.readObject().getJsonObject("ACCOUNTINFO");
-
-			response.getWriter().append("Account ")
-					.append(accountID)
-					.append(" has a balance of <abbr title=\"Updated via Servlet API call to CICS\" class=\"initialism\">$")
-					.append(jsonObject.getJsonNumber("Credit").toString()).append("</abbr>");
-
+			JsonObject jsonObject = jsonReader.readObject().getJsonObject("IVTNO_OUTPUT_MSG");
+			if (!"".equals(jsonObject.getString("OUT_LAST_NAME")))
+				response.getWriter()
+						.append("User <abbr title=\"IMSDB SEGNO=")
+						.append(jsonObject.getString("OUT_SEGNO"))
+						.append(" via Servlet API call\" class=\"initialism\">")
+						.append(jsonObject.getString("OUT_LAST_NAME"))
+						.append(",").append(jsonObject.getString("OUT_FIRST_NAME")).append("</abbr> Extension num:")
+						.append(jsonObject.getString("OUT_EXTENSION"))
+						.append(" Postcode: ").append(jsonObject.getString("OUT_ZIP_CODE"));
+			else
+				response.getWriter()
+						.append("User ").append(lastName)
+						.append(" not in the phonebook.");
 			in.close();
+			// String credit = payload.toString();
+			// credit = credit.substring(credit.indexOf("Credit") + 8,
+			// credit.indexOf("}}"));
+			// response.getWriter().append(
+			// "Account " + lastName + " has a balance of $" + credit);
 		} else {
-			response.getWriter().append("Account ")
-					.append(accountID).append(" not found. (HTTP_404)");
+			response.getWriter()
+					.append("User ").append(lastName)
+					.append(" not in the phonebook. HTTP_").append(String.valueOf(responseCode));
 		}
 	}
 }
--- src/main/webapp/backend.js
+++ src/main/webapp/backend.js
@@ -1,7 +1,9 @@
 
-const requestOne = "https://s0w1.duckdns.org:9445/catalog/items";
-const requestTwo = "https://s0w1.duckdns.org:9445/phonebook/contact/";
+const catalogUrl = "https://s0w1.duckdns.org:9445/catalog/items";
+const requestSingleUrl = "https://s0w1.duckdns.org:9445/catalog/item/";
 const authHeader = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiQmVhcmVyIiwiYXVkIjoiekNFRSIsInN1YiI6InlwZ2VAYXUxLmlibS5jb20iLCJyZWFsbSI6IkFEQ0RQTCIsImlzcyI6ImxpYmVydHkiLCJleHAiOjE2ODE0Nzk0NjksImlhdCI6MTU4MTQ3MjI2OX0.KJlE-wS1N48cWz1F1uDxpSN7uR9fb4uFGxY3o31OxuoQRFjYTXiaadeDzhmBrz0LK30bdi2oGZlfs0DEhFIkC8iwJlKRS4-bMudTLK37E_UW6T6EhjFmO-X8DUNJeiKVEz0rc982QFoGQLCm7T7-YT7HNqId2RFPe1JKWebJfH-zepME-R6CNpsu7f3ZWnJsopCz2ewuOtt72xtozf6ZnAXTUM46bp60vUbh7btha8C9XNHxq2pIKOJ6sJfcWjqjhA9M7oMRVYO_ml6Gpk6iRL_0HagMytjVEg6Veu7888f4K5IzQJaNQUTFVqE3S17o7Wn8dcCApf8D3CqM78WHag"
+const accountList = ["A1234567", "A7654321", "A0000000"];
+const lastNameList = ["GE", "MATTINGLY", "LNAME"];
 
 function listCICSItems(requestUrl) {
     fetch(requestUrl,
@@ -15,9 +17,9 @@ function listCICSItems(requestUrl) {
         })
         .then(res => res.json())
         .then((data) => {
-            $('#items-table > tbody').empty();
+            $('#items-table > tbody').fadeOut().empty();
             data["cics-data"].inventory.items.forEach((itemObj) => {
-                $('#items-table').append($('<tr>')
+                $('#items-table').append($('<tr id=' + itemObj.id + '>')
                     .append($('<th>').append(itemObj.id))
                     .append($('<td>').append(itemObj.desc))
                     .append($('<td>').append(itemObj.cost))
@@ -25,14 +27,14 @@ function listCICSItems(requestUrl) {
                 );
             }
             );
-            $('#btnLoadCICS').text('Get CICS Catalog Items').removeClass('disabled');
+            $('#items-table > tbody').fadeIn();
+            $('#btnLoadCICS').text('Refresh CICS Catalog').removeClass('disabled');
             $('#cics-footnote').text(new Date());
         })
 }
 
-function listIMSItems(requestUrl) {
-    requestUrl += $('#lastName').val().trim().toUpperCase();
-    fetch(requestUrl,
+function updateSingleItem(id) {
+    fetch(requestSingleUrl + id,
         {
             method: 'GET',
             mode: 'cors',
@@ -43,39 +45,50 @@ function listIMSItems(requestUrl) {
         })
         .then(res => res.json())
         .then((data) => {
-            $('#contact-table > tbody').empty();
-            itemObj = data["IVTNO_OUTPUT_MSG"];
-            $('#contact-table > tbody').append($('<tr>'))
-                .append($('<th>').append(itemObj["OUT_FIRST_NAME"]))
-                .append($('<td>').append(itemObj["OUT_LAST_NAME"]))
-                .append($('<td>').append(itemObj["OUT_EXTENSION"]))
-                .append($('<td>').append(itemObj["OUT_ZIP_CODE"]));
-            $('#btnLoadIMS').text('Lookup IMS').removeClass('disabled');
-            $('#ims-footnote').text("IMS MESSAGE: " + itemObj["OUT_MESSAGE"] + " @" + new Date());
+            $('#items-table > tbody > tr').removeClass("table-primary");
+            $('#' + id).fadeOut().fadeIn().addClass("table-primary");
         })
 }
+
 var timer = null;
 $(function () {
     $('#btnLoadCICS').click(function () {
         $('#btnLoadCICS').text('Loading').addClass('disabled');
-        listCICSItems(requestOne);
-    });
-    $('#btnLoadIMS').click(function () {
-        $('#btnLoadIMS').text('Loading').addClass('disabled');
-        listIMSItems(requestTwo);
+        listCICSItems(catalogUrl);
     });
     $('#switchRobot').click(function () {
         var act = $('#switchRobot').prop('checked');
         console.log("Robot status " + act);
         if (act) {
             timer = setInterval(() => {
-                if (Math.random()<0.3)
-                    $('#btnLoadIMS').click();
-                else
-                    $('#btnLoadCICS').click();
-            }, 1000);
+                var i = Math.floor(Math.random() * 100);
+                if (i <= 30)
+                    update("phonebookServlet?lname=" + lastNameList[i % lastNameList.length], $('#imsMessage'));
+                else if (i <= 60)
+                    update("accountServlet?account=" + accountList[i % accountList.length], $('#cicsMessage'));
+                else {
+                    items = $('#items-table > tbody > tr');
+                    selected = $(items[Math.floor(Math.random() * items.length)]).attr("id");
+                    updateSingleItem(selected);
+                }
+            }, 2000);
         } else
             clearInterval(timer);
-    })
+    });
+    listCICSItems(catalogUrl);
+    update("phonebookServlet?lname=GE", $('#imsMessage'));
+    update("accountServlet?account=A1234567", $('#cicsMessage'));
 });
 
+function update(url, obj) {
+    fetch(url /*, options */)
+        .then((response) => response.text())
+        .then((html) => {
+            obj.addClass("table-primary");
+            obj.fadeOut().html(html).fadeIn();
+            obj.removeClass("table-primary");
+        })
+        .catch((error) => {
+            console.warn(error);
+        });
+} 
